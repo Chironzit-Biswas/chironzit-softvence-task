0c7355ab5e4ba95bdfe3d672c510693b7ed3255b
Chironjit-Biswas <chironjit2@gmail.com>
2025-04-30 22:28:24 +0600

    softvence task

--- src/components/Aicoach.jsx
+++ src/components/Aicoach.jsx
@@ -1,5 +1,9 @@
 export const Aicoach = () => {
     return (
-        <></>
+        <div className="w-[1920px] bg-[#FAFAFA] py-[30px] bg-blue-700">
+             <div className="max-w-[1440px] mx-auto ">
+
+             </div>
+        </div>
     )
 }
